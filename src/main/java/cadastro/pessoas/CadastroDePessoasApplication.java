3da905e397292b2863865fae7b8144d905cd9e15
levybsousa <levybsousa02@gmail.com>
2024-01-17 22:24:59 -0300

    primeiro commit

---
+++ src/main/java/cadastro/pessoas/CadastroDePessoasApplication.java
@@ -0,0 +1,13 @@
+package cadastro.pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cadastro/pessoas/CadastroDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package cadastro.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
