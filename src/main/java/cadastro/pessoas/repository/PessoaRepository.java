12eae8175cbb149c18b269d2025fdc88f267db3e
levybsousa <levybsousa02@gmail.com>
2024-01-23 01:36:07 -0300

    concluindo interface PessoaRepository

--- src/main/java/cadastro/pessoas/repository/PessoaRepository.java
+++ src/main/java/cadastro/pessoas/repository/PessoaRepository.java
@@ -4,6 +4,9 @@ import cadastro.pessoas.model.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    Optional<Pessoa> getPessoaByNome(String nome);
 }
